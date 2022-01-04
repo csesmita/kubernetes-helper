8d6968c01e4597e46c2c241ae5be8888e013109f
Smita Vijayakumar <csesmita@users.noreply.github.com>
2022-01-04 20:06:58 +0000

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rediswq
 import os
+import re
 import subprocess
 from threading import Thread
 from datetime import timedelta, datetime
@@ -9,11 +10,7 @@ from collections import defaultdict
 from numpy import percentile
 
 job_to_podlist = defaultdict(list)
-SPEEDUP = 1
-sch_queue_add = "Add event for unscheduled pod"
-sch_queue_eject = "About to try and schedule pod"
-sch_attempt_schedule = "Attempting to schedule pod"
-sch_attempt_bind = "Attempting to bind pod to node"
+SPEEDUP = 10
 
 #end and start are of the form HH:MM:SS.SSSSSS
 def timeDiff(end, start):
@@ -48,20 +45,20 @@ def main():
             print "Next job due at", start_epoch + arrival_time, "but time now is", endTime
             raise AssertionError('script overran job interval!')
         sleep(sleep_time)
-        apply_job(filename, jobstr, start_epoch)
-        #thr = Thread(target=apply_job, args=(filename, jobstr, start_epoch), kwargs={})
-        #thr.start()
-        #threads.append(thr)
-        #thr.join()
+        #apply_job(filename, jobstr, start_epoch)
+        thr = Thread(target=apply_job, args=(filename, jobstr, start_epoch), kwargs={})
+        thr.start()
+        threads.append(thr)
 
     f.close()
 
     #Wait for all threads to start their respective jobs.
-    #for thr in threads:
-    #    thr.join()
+    for thr in threads:
+        thr.join()
 
     #Process scheduler stats.    
     stats()
+    print "Script took a total of", time() - start_epoch,"s"
 
 def apply_job(filename, jobstr, start_epoch):
         #This command takes about 0.25s. So jobs can't arrive faster than this.
@@ -72,11 +69,14 @@ def apply_job(filename, jobstr, start_epoch):
 def stats():
     pending_jobs = True
     #Change directory to scheduler logs
-    os.chdir("/var/lib/docker/containers/75a6972b0af20eefd1e68ac2735bcfcc579982570682a267e648be6f5e9e9eda")
+    os.chdir("/local/scratch/")
+    #Look for 12:14:58.422793 pattern in logs
+    pattern = '\d{2}:\d{2}:\d{2}\.\d{6}'
+    compiled = re.compile(pattern)
     while pending_jobs:
         pending_jobs = add_pod_info()
         print "Pending jobs?", pending_jobs
-        process()
+        process(compiled)
         sleep(2)
     print "Finished processing all jobs"
 
@@ -95,7 +95,8 @@ def add_pod_info():
         try:
             datetime.strptime(has_completed, '\'%Y-%m-%dT%H:%M:%SZ\'')
         except ValueError as e:
-            print "Job", jobname,"has not yet completed"
+            # This job has not yet completed.
+            print jobname,"has not completed yet. Got error", e
             continue
         # This job has completed.
         pods_list = subprocess.check_output(['kubectl','get', 'pods', '--selector=job-name='+jobname, '--server-print=false', '--no-headers'])
@@ -104,46 +105,69 @@ def add_pod_info():
             job_to_podlist[jobname].append(podname)
     return True
 
-#TODO - Docker should give read access to all users. For now, run sudo chmod 755 on /var/lib/docker.
-#TODO - Automate to pick up sym link from /var/log/pods/kube-system_kube-scheduler*/*/ directory. For now, hard code dir name.
-#TODO - Configue logrotate from docker instead of kubernetes. https://www.reddit.com/r/kubernetes/comments/bcoiwr/clearing_logs_log_rotation/. For now, it runs every 15 mmins.
-def process():
+QUEUE_ADD_LOG    = "Add event for unscheduled pod"
+QUEUE_DELETE_LOG = "Delete event for unscheduled pod"
+START_SCH_LOG    = "About to try and schedule pod"
+UNABLE_SCH_LOG   = "Unable to schedule pod"
+BIND_LOG         = "Attempting to bind pod to node"
+def process(compiled):
     for jobname,pods in job_to_podlist.items():
         # Fetch all pod related stats for each pod.
         for podname in pods:
-            queue_time =  0.0
+            #Two outputs for this pod
+            queue_time = timedelta(microseconds=0)
             scheduling_algorithm_time = timedelta(microseconds=0)
-            queue_add_time = timedelta(microseconds=0)
-            queue_eject_time = 0.0
-            attempt_schedule_time = 0.0
-            attempt_bind_time = 0.0
-            logs =  subprocess.check_output(['grep','-ri',podname]).split('\n')
+            #Intermittent points
+            queue_add_time = datetime.min
+            queue_eject_time = datetime.min
+            start_sch_time = datetime.min
+            unable_sch_time = datetime.min
+            attempt_bind_time = datetime.min
+            logs =  subprocess.check_output(['grep','-ri',podname, 'syslog']).split('\n')
             #Grep'ed logs can be out of order in time.
+            #sch_queue_eject and unable_sch may happen multiple times.
+            #TODO- Assumes sch_queue_eject log happens before unable_sch log, and they always happen one after the other. 
+            #TODO- That might not always be true.
             for log in logs:
-                if sch_queue_add in log:
-                    queue_add_time = log.split(' ')[1]
+                #This log happens exactly once
+                if QUEUE_ADD_LOG in log:
+                    if queue_add_time > datetime.min:
+                        print "Check calculcations for pod for queue add time", podname
+                        print "Job", jobname,"Pod", podname,"Scheduler Queue Time", timedelta(microsecond=0),"Scheduling Algorithm Time", timedelta(microsecond=0)
+                        break
+                    queue_add_time = datetime.strptime(compiled.search(log).group(0), '%H:%M:%S.%f')
                     continue
-                if sch_queue_eject in log:
-                    #if queue_add_time == 0.0:
-                    #    raise AssertionError('Got a queue eject time, but not a queue add time for pod ' + podname)
-                    queue_eject_time = log.split(' ')[1]
-                    queue_time = timeDiff(queue_eject_time, queue_add_time)
-                    continue
-                if sch_attempt_schedule in log:
-                    #if queue_time == 0.0:
-                    #    raise AssertionError('Got a attempt schedule time, but queue time has not yet been calculated for pod ' + podname)
-                    attempt_schedule_time = log.split(' ')[1]
-                    continue
-                if sch_attempt_bind in log:
-                    #if attempt_schedule_time == 0.0:
-                    #    raise AssertionError('Got a attempt bind time, but not a attempt schedule time for pod ' + podname)
-                    attempt_bind_time = log.split(' ')[1]
-                    scheduling_algorithm_time = timeDiff(attempt_bind_time, attempt_schedule_time)
-                    continue
-                if scheduling_algorithm_time.total_seconds() > 0.0 and queue_time.total_seconds() > 0.0:
+                #This log happens exactly once
+                if QUEUE_DELETE_LOG in log:
+                    queue_eject_time = datetime.strptime(compiled.search(log).group(0), '%H:%M:%S.%f')
+                    queue_time = queue_eject_time - queue_add_time
                     break
-            if scheduling_algorithm_time.total_seconds() == 0.0 or queue_time.total_seconds() == 0.0:
-                raise AssertionError('Scheduling algorithm time or queue time not calculated for pod ' + podname)
+                if START_SCH_LOG in log:
+                    if start_sch_time > datetime.min:
+                        print "Check calculcations for pod for start sch time", podname
+                        print "Job", jobname,"Pod", podname,"Scheduler Queue Time", timedelta(microsecond=0),"Scheduling Algorithm Time", timedelta(microsecond=0)
+                        break
+                    start_sch_time = datetime.strptime(compiled.search(log).group(0), '%H:%M:%S.%f')
+                    continue
+                if UNABLE_SCH_LOG in log:
+                    if start_sch_time == datetime.min:
+                        print "Check calculcations for pod for unschedulable pod time", podname
+                        print "Job", jobname,"Pod", podname,"Scheduler Queue Time", timedelta(microsecond=0),"Scheduling Algorithm Time", timedelta(microsecond=0)
+                        break
+                    unable_sch_time = datetime.strptime(compiled.search(log).group(0), '%H:%M:%S.%f')
+                    scheduling_algorithm_time = scheduling_algorithm_time + unable_sch_time - start_sch_time
+                    start_sch_time = datetime.min
+                    continue
+                #This log happens exactly once
+                if BIND_LOG in log:
+                    if start_sch_time == datetime.min:
+                        print "Check calculcations for pod for unschedulable pod time", podname
+                        print "Job", jobname,"Pod", podname,"Scheduler Queue Time", timedelta(microsecond=0),"Scheduling Algorithm Time", timedelta(microsecond=0)
+                        break
+                    attempt_bind_time = datetime.strptime(compiled.search(log).group(0), '%H:%M:%S.%f')
+                    scheduling_algorithm_time = scheduling_algorithm_time + attempt_bind_time - start_sch_time
+                    start_sch_time = datetime.min
+                    continue
             print "Job", jobname,"Pod", podname,"Scheduler Queue Time", queue_time,"Scheduling Algorithm Time", scheduling_algorithm_time
         subprocess.check_output(["kubectl", "delete", "jobs",jobname])
     job_to_podlist.clear()
