c04105340fa9bb5e7714cc17b86dfa9fa123dc2b
Smita Vijaya Kumar <sv440@cl.cam.ac.uk>
2022-01-11 10:53:32 +0000

    Adding delete jobs and create job files' scripts

--- delete_jobs.sh
+++ delete_jobs.sh
@@ -1,1 +1,7 @@
 kubectl delete jobs `kubectl get jobs -o custom-columns=:.metadata.name`
+rm -rf job[1-9]*.yaml
+echo $w1tch#123 | sudo rm /local/scratch/syslog
+sudo systemctl restart rsyslog
+echo "Number of pods still around = "
+kubectl get pods| wc -l
+echo "Check syslog is fine."
