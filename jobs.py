92d205cf197bc564fc162079669839bd04828751
Smita Vijaya Kumar <sv440@cl.cam.ac.uk>
2022-01-11 10:57:54 +0000

    Merged create script with jobs script

--- jobs.py
+++ jobs.py
@@ -19,7 +19,36 @@ def timeDiff(end, start):
     start_time = datetime.strptime(start, '%H:%M:%S.%f')
     return end_time - start_time
 
+def setup():
+    # Read in the job template file
+    with open('job.yaml', 'r') as file :
+        job_tmpl = file.read()
+    host = "10.104.213.87"
+    jobid = 0
+
+    # Process workload file to generate job files.
+    f = open('temp.tr', 'r')
+    for row in f:
+        row = row.split()
+        num_tasks = int(row[1])
+        est_time = float(row[2])
+        actual_duration = []
+        for index in range(num_tasks):
+            actual_duration.append(float(row[3+index]))
+        # Replace the template file with actual values
+        jobid += 1
+        jobstr = "job"+str(jobid)
+        filedata = job_tmpl.replace('$JOBID',jobstr).replace("$NUM_TASKS",str(num_tasks))
+        filename = jobstr+".yaml"
+        with open(filename, 'w') as file:
+          file.write(filedata)
+        q = rediswq.RedisWQ(name=jobstr, host=host)
+        q.rpush(actual_duration)
+    f.close()
+    print "Done generating job files."
+
 def main():
+    setup()
     jobid = 0
     start_epoch = 0.0
     threads = []
