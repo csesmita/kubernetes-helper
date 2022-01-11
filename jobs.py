70c7ae68d0884cdb5cbad70d2fbd7c9fcd6e92d3
Smita Vijaya Kumar <sv440@cl.cam.ac.uk>
2022-01-11 16:32:59 +0000

    Merged process and job python scripts

--- jobs.py
+++ jobs.py
@@ -3,6 +3,9 @@ import rediswq
 import os
 import re
 import subprocess
+import sys
+import subprocess
+import operator
 from threading import Thread
 from datetime import timedelta, datetime
 from time import sleep, time, mktime
@@ -13,42 +16,17 @@ from numpy import percentile
 job_to_podlist = {}
 SPEEDUP = 1
 
-#end and start are of the form HH:MM:SS.SSSSSS
-def timeDiff(end, start):
-    end_time = datetime.strptime(end, '%H:%M:%S.%f')
-    start_time = datetime.strptime(start, '%H:%M:%S.%f')
-    return end_time - start_time
+def extractDateTime(timestr):
+    return datetime.strptime(timestr,'%H:%M:%S.%f')
 
-def setup():
-    # Read in the job template file
-    with open('job.yaml', 'r') as file :
-        job_tmpl = file.read()
-    host = "10.104.213.87"
-    jobid = 0
-
-    # Process workload file to generate job files.
-    f = open('temp.tr', 'r')
-    for row in f:
-        row = row.split()
-        num_tasks = int(row[1])
-        est_time = float(row[2])
-        actual_duration = []
-        for index in range(num_tasks):
-            actual_duration.append(float(row[3+index]))
-        # Replace the template file with actual values
-        jobid += 1
-        jobstr = "job"+str(jobid)
-        filedata = job_tmpl.replace('$JOBID',jobstr).replace("$NUM_TASKS",str(num_tasks))
-        filename = jobstr+".yaml"
-        with open(filename, 'w') as file:
-          file.write(filedata)
-        q = rediswq.RedisWQ(name=jobstr, host=host)
-        q.rpush(actual_duration)
-    f.close()
-    print "Done generating job files."
+# Returns difference in milliseconds
+def timeDiffMilliseconds(e1, s1):
+    #e1 = extractDateTime(end)
+    #s1 = extractDateTime(start)
+    ms_diff = (mktime(e1.utctimetuple()) - mktime(s1.utctimetuple())) * 1000000 + e1.microsecond - s1.microsecond
+    return ms_diff * 1.0 / 1000
 
 def main():
-    setup()
     jobid = 0
     start_epoch = 0.0
     threads = []
@@ -88,7 +66,7 @@ def main():
         thr.join()
 
     #Process scheduler stats.    
-    stats()
+    stats(jobid)
     print "Script took a total of", time() - start_epoch,"s"
 
 def apply_job(filename, jobstr, start_epoch):
@@ -96,25 +74,22 @@ def apply_job(filename, jobstr, start_epoch):
         subprocess.check_output(["kubectl","apply", "-f", filename])
         print "Starting", jobstr, "at", time() - start_epoch
 
-def stats():
+def stats(num_jobs):
     pending_jobs = True
     #Change directory to scheduler logs
     os.chdir("/local/scratch/")
     #Look for 12:14:58.422793 pattern in logs
     pattern = '\d{2}:\d{2}:\d{2}\.\d{6}'
     compiled = re.compile(pattern)
+    log_file_name = '_'.join(["logs", "X"+str(SPEEDUP), str(num_jobs)])
     while pending_jobs:
         pending_jobs = add_pod_info()
-        print "Pending jobs?", pending_jobs
-        process(compiled)
+        process(compiled, log_file_name)
         sleep(5)
-    print "Finished processing all jobs"
-
 
 def add_pod_info():
     if len(job_to_podlist.keys()) == 0:
         return False
-    count = 0
     for jobname in job_to_podlist.keys():
         has_completed = subprocess.check_output(["kubectl", "get", "jobs", jobname, "-o","jsonpath='{.status.completionTime}'"])
         try:
@@ -129,22 +104,36 @@ def add_pod_info():
             #print jobname,"has not completed yet. Got error", e
             continue
         # This job has completed.
-        count += 1
-        pods_list = subprocess.check_output(['kubectl','get', 'pods', '--selector=job-name='+jobname, '--server-print=false', '--no-headers'])
+        pods_list = subprocess.check_output(['kubectl','get', 'pods', '--selector=job-name='+jobname, '--no-headers'])
         for pod in pods_list.splitlines():
-            podname = pod.split()[0]
+            podstrs = pod.split()
+            podname = podstrs[0]
+            status = podstrs[2]
+            if status == "OutOfpods" or status == "Evicted":
+                #Known error. This happens when etcd is slower than events in the cluster.
+                #However, the job has sucessfully completed. So, look for other pods.
+                continue
+            if status != "Completed":
+                raise AssertionError("Pod" + podname + "is not in a completed state!")
             job_to_podlist[jobname].append(podname)
-        #Utmost 5 completed jobs in each iteration.
-        if count == 5:
-            break
     return True
 
-QUEUE_ADD_LOG    = "Add event for unscheduled pod"
-QUEUE_DELETE_LOG = "Delete event for unscheduled pod"
-START_SCH_LOG    = "About to try and schedule pod"
-UNABLE_SCH_LOG   = "Unable to schedule pod"
-BIND_LOG         = "Attempting to bind pod to node"
-def process(compiled):
+def process(compiled, log_file_name):
+    QUEUE_ADD_LOG    = "Add event for unscheduled pod"
+    QUEUE_DELETE_LOG = "Delete event for unscheduled pod"
+    START_SCH_LOG    = "About to try and schedule pod"
+    UNABLE_SCH_LOG   = "Unable to schedule pod"
+    BIND_LOG         = "Attempting to bind pod to node"
+
+    default_time = timedelta(microseconds=0)
+    qtimes = []
+    pod_start = {}
+    pod_end = {}
+    pod_node = {}
+    pod_durations = {}
+    pod_qdiff= {}
+    algotimes = []
+    log_file = open(log_file_name, 'w')
     for jobname,pods in job_to_podlist.items():
         # Fetch all pod related stats for each pod.
         has_pods = False
@@ -153,7 +142,7 @@ def process(compiled):
             #Two outputs for this pod
             queue_time = timedelta(microseconds=0)
             scheduling_algorithm_time = timedelta(microseconds=0)
-            #Intermittent points
+            #Interim points
             queue_add_time = datetime.min
             queue_eject_time = datetime.min
             start_sch_time = datetime.min
@@ -163,57 +152,76 @@ def process(compiled):
             logs =  subprocess.check_output(['grep','-ri',podname, 'syslog']).split('\n')
             #Grep'ed logs can be out of order in time.
             #sch_queue_eject and unable_sch may happen multiple times.
-            #TODO- Assumes sch_queue_eject log happens before unable_sch log, and they always happen one after the other. 
-            #TODO- That might not always be true.
             for log in logs:
                 #This log happens exactly once
                 if QUEUE_ADD_LOG in log:
                     if queue_add_time > datetime.min:
-                        print "Check calculcations for pod for queue add time", podname,
-                        queue_time = timedelta(microsecond=0)
-                        scheduling_algorithm_time = timedelta(microsecond=0)
-                        break
-                    queue_add_time = datetime.strptime(compiled.search(log).group(0), '%H:%M:%S.%f')
+                        raise AssertionError("Check calculcations for pod for queue add time"+ podname)
+                    queue_add_time = extractDateTime(compiled.search(log).group(0))
                     continue
                 #This log happens exactly once
                 if QUEUE_DELETE_LOG in log:
-                    queue_eject_time = datetime.strptime(compiled.search(log).group(0), '%H:%M:%S.%f')
+                    queue_eject_time = extractDateTime(compiled.search(log).group(0))
                     print "Job", jobname,"Pod", podname,"Started", queue_add_time, "ended at", queue_eject_time,
                     queue_time = queue_eject_time - queue_add_time
                     break
                 if START_SCH_LOG in log:
                     if start_sch_time > datetime.min:
-                        print "Check calculcations for pod for start sch time", podname,
-                        queue_time = timedelta(microsecond=0)
-                        scheduling_algorithm_time = timedelta(microsecond=0)
-                        break
-                    start_sch_time = datetime.strptime(compiled.search(log).group(0), '%H:%M:%S.%f')
+                        raise AssertionError("Check calculcations for pod for start sch time"+ podname)
+                    start_sch_time = extractDateTime(compiled.search(log).group(0))
                     continue
                 if UNABLE_SCH_LOG in log:
                     if start_sch_time == datetime.min:
-                        print "Check calculcations for pod for unschedulable pod time", podname,
-                        queue_time = timedelta(microsecond=0)
-                        scheduling_algorithm_time = timedelta(microsecond=0)
-                        break
-                    unable_sch_time = datetime.strptime(compiled.search(log).group(0), '%H:%M:%S.%f')
+                        raise AssertionError("Check calculcations for pod for unschedulable pod time"+ podname)
+                    unable_sch_time = extractDateTime(compiled.search(log).group(0))
                     scheduling_algorithm_time = scheduling_algorithm_time + unable_sch_time - start_sch_time
                     start_sch_time = datetime.min
                     continue
                 #This log happens exactly once
                 if BIND_LOG in log:
                     if start_sch_time == datetime.min:
-                        print "Check calculcations for pod for unschedulable pod time", podname,
-                        queue_time = timedelta(microsecond=0)
-                        scheduling_algorithm_time = timedelta(microsecond=0)
-                        break
-                    attempt_bind_time = datetime.strptime(compiled.search(log).group(0), '%H:%M:%S.%f')
+                        raise AssertionError("Check calculcations for pod for unschedulable pod time"+ podname)
+                    attempt_bind_time = extractDateTime(compiled.search(log).group(0))
                     scheduling_algorithm_time = scheduling_algorithm_time + attempt_bind_time - start_sch_time
                     start_sch_time = datetime.min
                     nodename = log.split('node=')[1]
                     continue
             print "Scheduler Queue Time", queue_time,"Scheduling Algorithm Time", scheduling_algorithm_time, "Node", nodename
+            '''
+            pod_node[podname] = nodename
+            pod_end[podname] = queue_eject_time
+            pod_start[podname] = queue_add_time
+            logs = subprocess.check_output(['kubectl','logs',podname]).split('\n')
+            for log in logs:
+                if "Working on " not in log:
+                    continue
+                duration = float(log.split('Working on ')[1])
+                pod_durations[podname] = duration
+                break
+            #TODO - If Docker removes its files, then the following assertionn will hit.
+            #Instead, listen to job events and put back GC in kube-controller-manager.
+            if podname not in pod_durations.keys():
+                raise AssertionError('Check pod', podname,"logs. It has not worked on anything?")
+            '''
+            qdiff = timeDiffMilliseconds(queue_time, default_time)
+            #pod_qdiff[podname]=qdiff
+            qtimes.append(qdiff)
+            algotimes.append(timeDiffMilliseconds(scheduling_algorithm_time, default_time))
         if has_pods:
             del job_to_podlist[jobname]
+    qtimes.sort()
+    algotimes.sort()
+    print >> log_file, logname,": Total number of pods evaluated", len(qtimes)
+    print >> log_file, "Stats for Scheduler Queue Times -",percentile(qtimes, 50), percentile(qtimes, 90), percentile(qtimes, 99)
+    print >> log_file, logname,": Total number of pods evaluated", len(algotimes)
+    print >> log_file, "Stats for Scheduler Algorithm Times -"",",percentile(algotimes, 50), percentile(algotimes, 90), percentile(algotimes, 99)
+    '''
+    pods_ordered = (sorted(pod_start.items(), key=operator.itemgetter(1)))
+    for podname, start in pods_ordered:
+        if podname in pod_qdiff.keys():
+            print >> log_file, "Pod", podname,"started at", start, "in queue till", pod_end[podname],"assigned node", pod_node[podname], "duration", pod_durations[podname], "queue time", pod_qdiff[podname]
+    '''
+    log_file.close()
 
 if __name__ == '__main__':
     main()
