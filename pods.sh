ec6c21d649212489d1d540eaf10c5ce01516fcec
Smita Vijaya Kumar <sv440@caelum-306.cl.cam.ac.uk>
2022-06-04 17:15:39 +0100

    Print number of pods and podnames at the end of the filename. Also use fast grep options.

--- pods.sh
+++ pods.sh
@@ -4,6 +4,7 @@
 #the log creation timestamp, and writes them into a file.
 match="Work completed by this task"
 if [ "$#" -eq 1 ]; then
+    pods=()
     jobname=$1
     podprefix="${jobname}-"
     filename="${jobname}.txt"
@@ -11,12 +12,14 @@ if [ "$#" -eq 1 ]; then
     while read -r line; do
        if [[ $line == *${match}* ]]; then
            podname=( "$(cut -d' ' -f7 <<< ${line} | cut -d':' -f1)" )
-           echo "Got podname $podname"
+           pods+=("$podname")
            #return timestamp sorted logs for these pods.
-           logs=$(grep $podname /local/scratch/syslog.d |sort -k7)
+           logs=$(LC_ALL=C fgrep $podname /local/scratch/syslog |sort -k7)
            echo "$logs" >> $filename
        fi
-    done < <(grep $podprefix /local/scratch/syslog.d)
+    done < <(grep $podprefix /local/scratch/syslog)
+    echo "${#pods[@]}" >> $filename
+    printf "${pods[*]} \n" >> $filename
 else
     echo "Illegal number of arguments."
 fi
