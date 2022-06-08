c669e280a00da239c4ad525bac2aba6db69aad28
Smita Vijaya Kumar <sv440@caelum-306.cl.cam.ac.uk>
2022-06-08 15:04:39 +0100

    Correcting the Docker  daemon config for logging to syslog

--- remote-setup.sh
+++ remote-setup.sh
@@ -15,10 +15,7 @@ sudo mkdir /etc/docker
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
+  "log-driver": "syslog",
   "storage-driver": "overlay2"
 }
 EOF
