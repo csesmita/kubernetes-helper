35f8526d06675b150958cea7bc67cf64cad0a6a7
sv440 <sv440@localhost>
2022-06-09 08:04:06 -0600

    Making cni0 and flannel.1 delete optional commands when resetting

--- worker-reset.sh
+++ worker-reset.sh
@@ -1,3 +1,3 @@
-yes | sudo kubeadm reset && sudo iptables -F && sudo iptables -t nat -F && sudo iptables -t mangle -F && sudo iptables -X && sudo rm -rf /etc/cni/net.d && sudo rm -rf $HOME/.kube/config && sudo ip link set cni1 down && sudo ip link set flannel.1 down && sudo ip link delete cni0 && sudo ip link delete flannel.1 && sudo systemctl restart containerd && sudo systemctl restart kubelet
-sudo kubeadm join 128.110.217.163:6443 --token dvvu70.1i45jcj29okzytpm \
-	        --discovery-token-ca-cert-hash sha256:50e39475cbf4efbe78df12cf0ffbfd2cc6872e37b41a993cfaab7e3e71acd225
+yes | sudo kubeadm reset && sudo iptables -F && sudo iptables -t nat -F && sudo iptables -t mangle -F && sudo iptables -X && sudo rm -rf /etc/cni/net.d && sudo rm -rf $HOME/.kube/config; sudo ip link set cni1 down ; sudo ip link set flannel.1 down ; sudo ip link delete cni0 ; sudo ip link delete flannel.1 ; sudo systemctl restart containerd && sudo systemctl restart kubelet
+sudo kubeadm join 128.110.219.85:6443 --token tkutx2.igtmj6aansi9m8hg \
+	        --discovery-token-ca-cert-hash sha256:3299e195bac12fbc2c7358674de5b3c4e0d8c55d0c73944d6a29d9decf222e5f
