980528419d6bc766621582ae8ccebc15802ec1d5
sv440 <sv440@localhost>
2022-06-10 11:46:52 -0600

    Nits

--- delete_jobs.sh
+++ delete_jobs.sh
@@ -3,7 +3,7 @@ rm -rf job[1-9]*.yaml
 sudo rm /local/scratch/syslog
 sudo systemctl restart rsyslog
 echo "Ensuring node1 has NoSchedule Taint"
-kubectl get nodes -o custom-columns=NAME:.metadata.name,TAINTS:.spec.taints --no-headers | grep node1
+kubectl get nodes -o custom-columns=NAME:.metadata.name,TAINTS:.spec.taints --no-headers | grep node1.sv440
 echo "Number of pods still around = "
 kubectl get pods| wc -l
 echo "Check syslog is fine."
