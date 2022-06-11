62302176df8204cb875a1f4c95c499fc6212e8a5
Smita Vijaya Kumar <sv440@caelum-306.cl.cam.ac.uk>
2022-06-11 18:25:26 +0100

    Pod processing from syslog by using a one time grep and storing that in a temporary file for post processing.

--- pods.sh
+++ pods.sh
@@ -3,50 +3,36 @@
 #It then looks for logs of each of the pods, sorts them by
 #the log creation timestamp, and writes them into a file.
 match="Work completed by this task"
-start_scheduling="Add event for unscheduled pod"
-end_scheduling="Delete event for scheduled pod"
 if [ "$#" -eq 2 ]; then
+    pods=()
     jobname=$1
     numpods=$2
     podprefix="${jobname}-"
     filename="${jobname}.txt"
+    tempfile="/local/scratch/temp-${jobname}"
     rm -f $filename || true
     count=0
-    #These are the log lines per pod we are interested in -
-    #1(Add event for unscheduled pod)
-    #*(About to try and schedule pod)
-    #*(Unable to schedule pod)
-    #1(Attempting to bind pod)
-    #1(Delete event for unscheduled pod)
-    #1(Added pod to worker queue)
-    #1(Ejecting pod from worker queue)
-    while read -r line; do
+    # Find all logs of this job. Expensive. So we write this into a file.
+    LC_ALL=C fgrep $podprefix /local/scratch/syslog > $tempfile
+    # Find all accountable pods of this job.
+    while read line; do
        if [[ $line == *${match}* ]]; then
-           podname=( "$(cut -d' ' -f6 <<< ${line} | cut -d':' -f1)" )
-           #We need timestamp sorted logs for these pods.
-           #But we cannot use a simple maxnum param in grep for this.
-           #like so - logs=$(LC_ALL=C fgrep -m $maxnum $podname /local/scratch/syslog |sort -k7)
-           #because there are some logs that can occur multiple times like
-           #about to try and schedule pod and unable to schedule pod.
-           #So, look for line numbers of start and end of scheduling for this pod
-           #(add event for unscheduled and delete event for unscheduled). 
-           # Only grep for scheduler logs in between these lines.
-           startline=$(grep -n -m 1 "$start_scheduling.*$podname"  /local/scratch/syslog|awk -F":" {'print $1'})
-           endline=$(grep -n -m 1 "$end_scheduling.*$podname"  /local/scratch/syslog |awk -F":" {'print $1'})
-           #Keep some buffer on the starting edge as well.
-           start=$startline
-           if (($startline > 100)); then
-               start=$(($startline - 100))
-           fi
-           logs=$(tail -n +$start /local/scratch/syslog |head -n $(($endline - $start)) | LC_ALL=C fgrep $podname | sort -k6)
-           echo "Logs for $podname" >> $filename
-           echo "$logs" >> $filename
+           podname=$(echo "${line}"| sed 's/^.*\('"$podprefix"'.*\):.*$/\1/')
+           pods+=($podname)
            ((count++))
            if [ $count -eq $numpods ]; then
                break
            fi
        fi
-    done < <(LC_ALL=C fgrep $podprefix /local/scratch/syslog)
+    done <$tempfile
+    #For each pod, time sort its logs and write into final file.
+    for podname in "${pods[@]}"; do
+        #Grep for all logs with podname and sort them.
+        logs=$(LC_ALL=C fgrep $podname $tempfile | sort -k7)
+        echo "Logs for $podname" >> $filename
+        echo "$logs" >> $filename
+    done
+    rm $tempfile
 else
     echo "Illegal number of arguments."
 fi
