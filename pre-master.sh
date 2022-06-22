7b671c2df7e974a22c553323c4300213e443a3e4
sv440 <sv440@localhost>
2022-06-22 12:12:27 -0600

    Nit - correct id_ed files' path

--- pre-master.sh
+++ pre-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #git clone https://github.com/csesmita/kubernetes-helper.git
-mv id_ed* ~/.ssh/
+mv ~/id_ed* ~/.ssh/
 git config --global credential.helper store
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
