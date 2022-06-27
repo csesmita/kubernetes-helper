bba7dc504028dc771e860f59a8e998a57a153e4e
Smita Vijaya Kumar <sv440@cl.cam.ac.uk>
2022-06-27 16:05:43 +0100

    Post run script to run on airwolf when cloudlab run finishes.

--- post-run.sh
+++ post-run.sh
@@ -5,9 +5,14 @@ if [ "$#" -ne 1 ]; then
 fi
 node=$1
 cd ~/kubernetes-helper
-scp $node:/local/scratch/syslog results/
 scp $node:d.* results/jrt/
 scp $node:c.* results/jrt/
 scp $node:pods.d.* results/pods/
 scp $node:pods.c.* results/pods/
 scp $node:node_utilization.txt results/utilization/
+
+scp $node:/local/scratch/syslog results/
+scp results/syslog caelum-306:/local/scratch/syslog.xxxx
+echo "Change the name of the syslog file on caelum-306."
+echo "Press any key when done."
+read  -n 1
