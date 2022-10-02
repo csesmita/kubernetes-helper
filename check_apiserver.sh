cffbcc0bf40c3f59333442fd79c94994c207fa61
sv440 <sv440@localhost>
2022-10-02 11:06:00 -0600

    Adding HAProxy and Keepalived config files

--- check_apiserver.sh
+++ check_apiserver.sh
@@ -5,7 +5,7 @@ errorExit() {
 	        exit 1
 	}
 
-curl --silent --max-time 2 --insecure https://localhost:6443/ -o /dev/null || errorExit "Error GET https://localhost:6443/"
-if ip addr | grep -q 128.232.80.200; then
-	    curl --silent --max-time 2 --insecure https://128.232.80.200:8443/ -o /dev/null || errorExit "Error GET https://128.232.80.200:8443/"
+curl --silent --max-time 2 --insecure https://localhost:8443/ -o /dev/null || errorExit "Error GET https://localhost:8443/"
+if ip addr | grep -q 128.110.223.200; then
+	    curl --silent --max-time 2 --insecure https://128.110.223.200:8443/ -o /dev/null || errorExit "Error GET https://128.110.223.200:8443/"
 fi
