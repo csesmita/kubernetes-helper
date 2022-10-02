a6624f107d42371080dde0f05ff5dc323bc9448e
sv440 <sv440@localhost>
2022-10-02 01:31:48 -0600

    Script for calculating the rate of job creation and pod creation at Kube Controller and Scheduler coming from syslog.

---
+++ rate_jobs_pods.py
@@ -0,0 +1,59 @@
+import re
+from datetime import datetime, timedelta
+
+pattern = '\d{2}\d{2} \d{2}:\d{2}:\d{2}\.\d{6}'
+compiled = re.compile(pattern)
+jobs_added = {}
+pods_added = {}
+pods_unscheduled = {}
+j_event_string = "Adding job default"
+c_event_string = " created pod "
+s_event_string = "Add event for unscheduled pod"
+fr= datetime.min
+l=datetime.min
+
+with open("/local/scratch/syslog.jobs",'r') as f:
+    for line in f:
+        jobname = line.split(j_event_string)[1].split()[0]
+        jobs_added[jobname] = datetime.strptime(compiled.search(line).group(0),'%m%d %H:%M:%S.%f')
+        if fr == datetime.min:
+            fr = jobs_added[jobname]
+        l = jobs_added[jobname]
+
+num_pods = len(jobs_added.keys())
+print("Number of jobs created at input of controller", num_pods)
+print("Started creating at", fr, "and ended at", l)
+print("Creation Time Per Job At Controller = ", ((l - fr).total_seconds()) / num_pods)
+print("-----------------")
+
+fr= datetime.min
+l=datetime.min
+with open("/local/scratch/syslog.controller",'r') as f:
+    for line in f:
+        podname = line.split(c_event_string)[1].split()[0]
+        pods_added[podname] = datetime.strptime(compiled.search(line).group(0),'%m%d %H:%M:%S.%f')
+        if fr == datetime.min:
+            fr = pods_added[podname]
+        l = pods_added[podname]
+
+num_pods = len(pods_added.keys())
+print("Number of pods created by controller", num_pods)
+print("Started creating at", fr, "and ended at", l)
+print("Creation Time Per Pod At Controller = ", ((l - fr).total_seconds()) / num_pods)
+print("-----------------")
+
+fr= datetime.min
+l=datetime.min
+with open("/local/scratch/syslog.scheduler",'r') as f:
+    for line in f:
+        podname = line.split(s_event_string)[1].split()[1]
+        pods_unscheduled[podname] = datetime.strptime(compiled.search(line).group(0),'%m%d %H:%M:%S.%f')
+        if fr == datetime.min:
+            fr = pods_unscheduled[podname]
+        l = pods_unscheduled[podname]
+
+num_pods = len(pods_unscheduled.keys())
+print("Number of unscheduled pods", num_pods)
+print("Started scheduling at", fr, "and ended at", l)
+print("Time Per Pod At Scheduler = ", ((l - fr).total_seconds()) / num_pods)
+print("-----------------")
