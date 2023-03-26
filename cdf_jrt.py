62d6cc5bdac84d9d2cfcd155f9f160cd6b9d21f1
Smita Vijaya Kumar <sv440@cl.cam.ac.uk>
2023-03-26 11:14:01 +0100

    Original JRT plot

--- cdf_jrt.py
+++ cdf_jrt.py
@@ -11,7 +11,7 @@ colors = Set2_7.mpl_colors
 def addlabels(y, pos):
     for i in range(len(y)):
         #plt.text(i + pos,y[i], y[i], ha = 'center', Bbox = dict(facecolor = 'red', alpha =.8))
-        plt.text(i + pos,y[i], y[i], ha = 'right', fontsize='x-small', color='dimgray')
+        plt.text(i + i*pos,y[i], "{:.2f}".format(y[i]), ha = 'right', fontsize='x-small', color='dimgray')
 
 c=collections.defaultdict(list)
 with open("results/jrt/c.10000J.200X.50N.YH", 'r') as f:
@@ -94,19 +94,20 @@ params = {
    'xtick.labelsize': 10,
    'ytick.labelsize': 10,
    'text.usetex': False,
-   #'figure.figsize': [10, 3]
+   'figure.figsize': [19.2, 4.8]
 }
 rcParams.update(params)
 #Show Percentiles
 percentiles=['50', '90', '99']
-x=np.arange(len(percentiles))
+tps=['400', '1200', '2000']
+x=np.arange(len(tps))
 width=0.35
 fig, ax = plt.subplots()
-yc_200= [int(np.percentile(c[200], 50)), int(np.percentile(c[200], 90)), int(np.percentile(c[200], 99))]
-yd_200= [int(np.percentile(d[200], 50)), int(np.percentile(d[200], 90)), int(np.percentile(d[200], 99))]
-y=[int(100*(j-i)/i) for i,j in zip(yd_200, yc_200)]
-rects1= ax.bar(x-width/3 + x, y, width, label="400 TPS", color=colors[0])
-#addlabels(y, 1)
+yc_50= [int(np.percentile(c[200], 50)), int(np.percentile(c[600], 50)), int(np.percentile(c[1000], 50))]
+yd_50= [int(np.percentile(d[200], 50)), int(np.percentile(d[600], 50)), int(np.percentile(d[1000], 50))]
+y=[((j)/i) for i,j in zip(yd_50, yc_50)]
+rects1= ax.bar(x-width/3 + x, y, width, label="50th %ile", color=colors[0])
+#addlabels(y, width)
 #rects2= ax.bar(x+width/2, y,  width, label="Decentralized 400 TPS", color=colors[1])
 #addlabels(y, 0)
 
@@ -119,10 +120,10 @@ rects2= ax.bar(x+width/2 + 4, y, width, label="Decentralized 400X")
 addlabels(y,4)
 '''
 
-yc_600= [int(np.percentile(c[600], 50)), int(np.percentile(c[600], 90)), int(np.percentile(c[600], 99))]
-yd_600= [int(np.percentile(d[600], 50)), int(np.percentile(d[600], 90)), int(np.percentile(d[600], 99))]
-y=[int(100*(j-i)/i) for i,j in zip(yd_600, yc_600)]
-rects1= ax.bar(x + 2*width/3 + x, y, width, label="1200 TPS",color=colors[2])
+yc_90= [int(np.percentile(c[200], 90)), int(np.percentile(c[600], 90)), int(np.percentile(c[1000], 90))]
+yd_90= [int(np.percentile(d[200], 90)), int(np.percentile(d[600], 90)), int(np.percentile(d[1000], 90))]
+y=[((j)/i) for i,j in zip(yd_90, yc_90)]
+rects1= ax.bar(x + 2*width/3 + x, y, width, label="90th %ile",color=colors[2])
 #addlabels(y,2)
 #rects2= ax.bar(x+width/2 + 8, y, width, label="Decentralized 1200 TPS", color=colors[3])
 #addlabels(y,8)
@@ -136,22 +137,23 @@ rects2= ax.bar(x+width/2 + 12, y, width, label="Decentralized 800X")
 addlabels(y,12)
 '''
 
-yc_1000= [int(np.percentile(c[1000], 50)), int(np.percentile(c[1000], 90)), int(np.percentile(c[1000], 99))]
-yd_1000= [int(np.percentile(d[1000], 50)), int(np.percentile(d[1000], 90)), int(np.percentile(d[1000], 99))]
-y=[int(100*(j-i)/i) for i,j in zip(yd_1000, yc_1000)]
-rects1= ax.bar(x+ 5*width/3 + x, y, width, label="2000 TPS",color=colors[4])
+yc_99= [int(np.percentile(c[200], 99)), int(np.percentile(c[600], 99)), int(np.percentile(c[1000], 99))]
+yd_99= [int(np.percentile(d[200], 99)), int(np.percentile(d[600], 99)), int(np.percentile(d[1000], 99))]
+y=[((j)/i) for i,j in zip(yd_99, yc_99)]
+rects1= ax.bar(x+ 5*width/3 + x, y, width, label="99th %ile",color=colors[4])
 #addlabels(y,3)
 #rects2= ax.bar(x+width/2 + 16, y, width, label="Decentralized 2000 TPS", color=colors[5])
 #addlabels(y,16)
 
 
 ax.set_ylabel("Relative JCT")
-plt.title("JCT in Kubernetes relative to Murmuration for Different Task Rates")
-ax.set_xlabel("Percentiles")
+plt.title("JCT in Kubernetes relative to Murmuration")
+ax.set_xlabel("Tasks Per Second")
 ax.set_xticks([0+2*width/3,2+2*width/3,4+2*width/3])
 #ax.set_xticklabels(['200X', '400X', '600X', '800X', '1000X'])
-ax.set_xticklabels(['50th', '90th', '99th'])
+ax.set_xticklabels(['400', '1200', '2000'])
 #ax.set_ylim([25000, 45000])
+ax.set_yticks([0.5, 1.0, 1.5])
 #plt.legend(fontsize='small', ncol=2, labelspacing=0.05)
 plt.legend(fontsize='small', loc='best')
 fig.tight_layout()
