4dbf2dbd4ccf8bdde8f1ac3d6e9b4c1b45c6f20c
Smita Vijayakumar <csesmita@users.noreply.github.com>
2023-04-29 06:42:04 +0100

    Update master.sh

--- master.sh
+++ master.sh
@@ -83,6 +83,7 @@ echo "For scalability experiments, switch off syslogging in rsyslog conf files. 
 
 echo "If running C then delete all schedulers using kubectl delete -f scheduler_configs/. Copy in kubelet.c and run switch_kubelet.sh."
 echo "If running D then copy in kubelet and run switch_kubelet.sh. Let C scheduler remain as it schedules system pods. Also, check if desired number of schedulers are present in scheduler_config and are running."
+echo "Update logging levels in scheduler config files, C and D"
 echo "C or D, change scheduler, kubelet and, job_c and job_d images if need be (jobbatch, none, etc.)"
 echo "SSH to node1. Create the temp file and change the rate of job arrivals in job.py. If need be, the runninng times of the jobs as well. Check num_cpus in jobs.py. Increase for faster rates, else let 1 be for large scale scalability expts."
 echo "Also, ensure you start a screen on node1 before starting experiments. Press yes when asked about screen. CHeck if jobs are created after starting the script."
