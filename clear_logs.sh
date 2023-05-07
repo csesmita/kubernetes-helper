8e9886dd98dff99642c88aef6db63bc117b2d59f
Smita Vijaya Kumar <sv440@cl.cam.ac.uk>
2023-05-07 11:37:41 +0100

    Restart kubelet as a part of clearing workers

--- clear_logs.sh
+++ clear_logs.sh
@@ -1,4 +1,6 @@
 sudo rm /local/scratch/syslog
 sudo systemctl restart docker
 sudo systemctl restart rsyslog
+sudo systemctl stop kubelet
+sudo systemctl start kubelet
 yes | sudo docker system prune -a
